d2428c9f4a2f840a3b6627e1fb7f663c2b19d4eb
flamingFennec <kattycakey5790@gmail.com>
2022-11-28 12:40:35 -0500

    leap date

---
+++ leap year.py
@@ -0,0 +1,9 @@
+year = int(input("What is the year? "))
+if (year % 4) == 0:
+    if (year % 100) == 0:
+     if (year % 400) == 0:
+         print ("Leap year.")
+     else:
+        print("Not leap year.")
+else:
+    print("Not leap year.")
