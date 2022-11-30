eaa8ca1b45e1673a72e05d4e85e03ab30e3eecd8
flamingFennec <kattycakey5790@gmail.com>
2022-11-30 11:18:08 -0500

    leap year fix

--- leap year.py
+++ leap year.py
@@ -1,9 +1,11 @@
 year = int(input("What is the year? "))
 if (year % 4) == 0:
     if (year % 100) == 0:
-     if (year % 400) == 0:
-         print ("Leap year.")
-     else:
+        if (year % 400) == 0:
+            print ("Leap year.")
+        else:
+            print ("Not a leap year.")
+    else:
         print("Not leap year.")
 else:
     print("Not leap year.")
